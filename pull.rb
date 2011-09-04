597605c80d0c8f4cebdd6050afef097b28f7ced9
Curtis McEnroe <programble@gmail.com>
2011-09-04 14:08:01 -0400

    Added magical script

---
+++ pull.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+dotfiles = {}
+
+dirs = ['.']
+dirs.each do |dir|
+  Dir.foreach(dir) do |file|
+    next if file[0] == '.'
+    file = File.join(dir, file)
+    if File.directory? file
+      dirs << file
+      next
+    end
+    dotfiles[file] = file.sub('.', '~').sub('_', '.') if %r{/_} =~ file
+  end
+end
+
+dotfiles.each do |a, b|
+  a, b = File.expand_path(a), File.expand_path(b)
+  FileUtils.cp(b, a)
+  puts "#{b} -> #{a}"
+end
+
+system('git add .')
+exec('git commit -a')
