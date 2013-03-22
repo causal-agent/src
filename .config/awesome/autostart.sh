30a83ab7355cf43df4a439cf0fe48aaef4bbbdc3
June McEnroe <june@causal.agency>
2013-03-22 00:09:16 -0400

    Simplify autostart

--- .config/awesome/autostart.sh
+++
@@ -1,4 +0,0 @@
-xsetroot -cursor_name left_ptr &
-#xcompmgr -c -f -D 3 -C &
-/home/home/code/c/keycounter/keycounter -f -d -p /home/home/.keycounter.pid /home/home/.keycount &
-gitdocs start &
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -336,5 +336,3 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-
-awful.util.spawn(awesome_home .. "/autostart.sh")
