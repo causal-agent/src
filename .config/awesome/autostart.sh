eab0003768ae4ebe6b3ba5f0f25cce8e2f81f7ce
Curtis McEnroe <programble@gmail.com>
2013-03-22 00:09:16 -0400

    Simplify autostart

--- .config/awesome/autostart.sh
+++
@@ -1,4 +0,0 @@
-xsetroot -cursor_name left_ptr &
-#xcompmgr -c -f -D 3 -C &
-/home/curtis/code/c/keycounter/keycounter -f -d -p /home/curtis/.keycounter.pid /home/curtis/.keycount &
-gitdocs start &
