9debeec5079a124ce6edc8b17bd6a0d00e960949
Curtis McEnroe <programble@gmail.com>
2013-05-04 14:17:49 -0400

    Update awesome theme

--- .config/awesome/themes/Tomorrow-Night/theme.lua
+++ .config/awesome/themes/Tomorrow-Night/theme.lua
@@ -10,13 +10,14 @@ theme.bg_normal     = "#1d1f2100"
 theme.bg_focus      = "#373b4155"
 theme.bg_urgent     = "#1d1f2155"
 theme.bg_minimize   = "#282a2e"
+theme.bg_systray    = "#5A6290"
 
 theme.fg_normal     = "#c5c8c6"
 theme.fg_focus      = "#c5c8c6"
 theme.fg_urgent     = "#cc6666"
 theme.fg_minimize   = "#c5c8c6"
 
-theme.border_width  = "1"
+theme.border_width  = 1
 theme.border_normal = "#1d1f21"
 theme.border_focus  = "#282a2e"
 theme.border_marked = "#373b41"
@@ -32,17 +33,15 @@ theme.border_marked = "#373b41"
 --theme.taglist_bg_focus = "#ff0000"
 
 -- Display the taglist squares
-theme.taglist_squares_sel   = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/usr/share/awesome/themes/default/taglist/squarew.png"
-
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+theme.taglist_squares_sel   = ".config/awesome/themes/Tomorrow-Night/taglist/squarefz.png"
+theme.taglist_squares_unsel = ".config/awesome/themes/Tomorrow-Night/taglist/squarez.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
-theme.menu_height = "15"
-theme.menu_width  = "100"
+theme.menu_height = 15
+theme.menu_width  = 100
 
 -- You can add as many variables as
 -- you wish and access them by using
@@ -73,8 +72,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
--- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "nitrogen --restore" }
+theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
@@ -92,5 +90,9 @@ theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
+-- Define the icon theme for application icons. If not set then the icons 
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
 return theme
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
