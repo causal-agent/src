3a6a3b7bcc570899b1814106ca5670b0d49b198a
Curtis McEnroe <curtis.mcenroe@adgear.com>
2016-09-12 14:52:25 -0400

    Add -c option to xx

--- .bin/xx.c
+++ .bin/xx.c
@@ -1,17 +1,24 @@
 #if 0
-exec cc -Weverything -o ~/.bin/xx $0
+exec cc -Weverything -Wno-vla -o ~/.bin/xx $0
 #endif
 
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
+    size_t cols = 16;
     char *path = NULL;
 
-    if (argc > 1)
-        path = argv[1];
+    while (getopt(argc, argv, "c:") > 0)
+        if (optopt == 'c') {
+            cols = (size_t) strtol(optarg, NULL, 10);
+            if (!cols) return EXIT_FAILURE;
+        } else return EXIT_FAILURE;
+    if (argc > optind)
+        path = argv[optind];
 
     FILE *file = path ? fopen(path, "r") : stdin;
     if (!file) {
@@ -19,7 +26,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    uint8_t buf[16];
+    uint8_t buf[cols];
     for (;;) {
         size_t n = fread(buf, 1, sizeof(buf), file);
         for (size_t i = 0; i < n; ++i)
