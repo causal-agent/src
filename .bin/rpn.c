4404a234bcd56c2c049db8197891ca2aef877647
Nicole Mazzuca <npmazzuca@gmail.com>
2016-10-05 00:37:21 -0400

    Update rpn.c

--- .bin/rpn.c
+++ .bin/rpn.c
@@ -68,72 +68,52 @@ static bool stack_op(char op) {
     switch (op) {
         case '@':
         case '\'':
-        case '"': {
-            stack.op = op;
-        } break;
-        case 'b': {
-            stack.radix = 2;
-        } break;
-        case 'o': {
-            stack.radix = 8;
-        } break;
-        case 'd': {
-            stack.radix = 10;
-        } break;
-        case 'x': {
-            stack.radix = 16;
-        } break;
-        case ';': {
-            a = pop();
-        } break;
+        case '"': stack.op = op;
+            break;
+        case 'b': stack.radix = 2;
+            break;
+        case 'o': stack.radix = 8;
+            break;
+        case 'd': stack.radix = 10;
+            break;
+        case 'x': stack.radix = 16;
+            break;
+        case ';': a = pop();
+            break;
         case ':': a = pop(); push(a); push(a);
-        break;
-        case '\\': {
-            a = pop(); b = pop(); push(a); push(b);
-        break;
-        case '_': {
-            a = pop(); push(-a);
-        break;
-        case '+': {
-            a = pop(); push(pop() + a);
-        break;
-        case '-': {
-            a = pop(); push(pop() - a);
-        break;
-        case '*': {
-            a = pop(); push(pop() * a);
-        break;
-        case '/': {
-            a = pop(); push(pop() / a);
-        break;
-        case '%': {
-            a = pop(); push(pop() % a);
-        break;
-        case '!': {
-            a = pop(); push(!a);
-        break;
-        case '&': {
-            a = pop(); push(pop() & a);
-        break;
-        case '|': {
-            a = pop(); push(pop() | a);
-        break;
-        case '^': {
-            a = pop(); push(pop() ^ a);
-        break;
-        case '<': {
-            a = pop(); push((unsigned long) pop() << a);
-        break;
-        case '>': {
-            a = pop(); push((unsigned long) pop() >> a);
-        break;
-        case '.': {
-            a = pop(); printf("%s\n", fmt(stack.radix, a));
-        break;
-        case ',': {
-            a = pop(); printf("%c\n", (char) a);
-        break;
-        case ' ': /* do nothing */ break;
+            break;
+        case '\\': a = pop(); b = pop(); push(a); push(b);
+            break;
+        case '_': a = pop(); push(-a);
+            break;
+        case '+': a = pop(); push(pop() + a);
+            break;
+        case '-': a = pop(); push(pop() - a);
+            break;
+        case '*': a = pop(); push(pop() * a);
+            break;
+        case '/': a = pop(); push(pop() / a);
+            break;
+        case '%': a = pop(); push(pop() % a);
+            break;
+        case '!': a = pop(); push(!a);
+            break;
+        case '&': a = pop(); push(pop() & a);
+            break;
+        case '|': a = pop(); push(pop() | a);
+            break;
+        case '^': a = pop(); push(pop() ^ a);
+            break;
+        case '<': a = pop(); push((unsigned long) pop() << a);
+            break;
+        case '>': a = pop(); push((unsigned long) pop() >> a);
+            break;
+        case '.': a = pop(); printf("%s\n", fmt(stack.radix, a));
+            break;
+        case ',': a = pop(); printf("%c\n", (char) a);
+            break;
+        case ' ': // do nothing
+            break;
         default: return false;
     }
     return true;
