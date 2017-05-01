21619267718ce86e1d1ad89fb491ec9713943679
June McEnroe <june@causal.agency>
2017-05-01 17:41:57 -0400

    Fix prune script loop
    For loop was getting one string with several paths separated by
    newlines.

--- prune.sh
+++ prune.sh
@@ -4,9 +4,7 @@
 
 set -o errexit -o nounset -o pipefail
 
-links=$(find -L ~ -type l -lname "$PWD/*")
-
-for link in $links; do
+find -L ~ -type l -lname "$PWD/*" | while read link; do
   rm "$link"
   echo "$link"
 done
