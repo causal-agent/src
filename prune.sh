da30c5658b8d1d10969fdbeb2866cd0adccee94c
June McEnroe <june@causal.agency>
2017-05-01 17:32:07 -0400

    Fix prune script
    The for loop was overriding zsh's $path variable, causing it to not be
    able to find rm.

--- prune.sh
+++ prune.sh
@@ -4,9 +4,9 @@
 
 set -o errexit -o nounset -o pipefail
 
-paths=$(find -L ~ -type l -lname "$PWD/*")
+links=$(find -L ~ -type l -lname "$PWD/*")
 
-for path in $paths; do
-  rm "$path"
-  echo "$path"
+for link in $links; do
+  rm "$link"
+  echo "$link"
 done
