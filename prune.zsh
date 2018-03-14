d7842c2b96da32bcfb181a88c34a4b67cf6bc815
June McEnroe <june@causal.agency>
2018-03-14 00:10:56 -0400

    Combine home-related scripts

--- prune.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-set -o errexit -o nounset -o pipefail
-
-# Remove broken symbolic links in ~.
-
-find -L ~ -type l -lname "$PWD/*" | while read link; do
-    rm "$link"
-    echo "$link"
-done
