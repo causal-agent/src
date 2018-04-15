c9ef9b09cbd67ea41fe353f21d92c6c87aa39b48
June McEnroe <june@causal.agency>
2018-04-15 15:01:28 -0400

    Add TF2 configs

---
+++ etc/tf/link.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+set -o errexit -o nounset
+
+tf="$HOME/Library/Application Support/Steam/steamapps/common/Team Fortress 2/tf"
+for cfg in cfg/*.cfg; do
+    ln -s -f "$PWD/$cfg" "$tf/$cfg"
+done
