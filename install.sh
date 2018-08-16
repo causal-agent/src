31e697c1ca8e523dd4a83ae16782b8d7e7190ace
June McEnroe <june@causal.agency>
2018-08-16 18:10:42 -0400

    Install mksh rather than zsh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 set -e -u
 
-any='gnupg htop neovim sl the_silver_searcher tree'
+any='gnupg htop mksh neovim sl the_silver_searcher tree'
 brew="$any ddate git openssh"
-pkg="$any curl ddate sudo zsh"
-pacman="$any base-devel ctags gdb openssh zsh"
+pkg="$any curl ddate sudo"
+pacman="$any base-devel ctags gdb openssh"
 
 homebrew='https://raw.githubusercontent.com/Homebrew/install/master/install'
 if [ "$(uname)" = 'Darwin' ]; then
 	xcode-select --install || true
 	[ -f /usr/local/bin/brew ] || ruby -e "$(curl -fsSL "$homebrew")"
-	exec brew install $brew
+	brew install $brew || true
+	if ! grep -q 'mksh' /etc/shells; then
+		echo '/usr/local/bin/mksh' | sudo tee -a /etc/shells > /dev/null
+	fi
+	exit
 fi
 
 [ -f /usr/local/sbin/pkg ] && exec pkg install $pkg
