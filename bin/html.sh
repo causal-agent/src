5d9fbeab91677ccac995337daf530e8a2d83fd68
June McEnroe <june@causal.agency>
2019-02-20 20:07:32 -0500

    Use $PWD in html.sh to pass absolute path to man

--- bin/html.sh
+++ bin/html.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-readonly GiteaURL=https://code.causal.agency/june/src/src/branch/master/bin
+readonly GiteaURL='https://code.causal.agency/june/src/src/branch/master/bin'
 
 src=$1
 man=${2:-}
@@ -10,5 +10,5 @@ man=${2:-}
 cat <<- EOF
 	<code><a href="${GiteaURL}/${src}">${src} in git</a></code>
 EOF
-[ -f "${man}" ] && man -P cat "${man}" | ./ttpre
+[ -f "${man}" ] && man -P cat "${PWD}/${man}" | ./ttpre
 ./hi -f html -o anchor "${src}"
