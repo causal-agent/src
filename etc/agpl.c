88aebda7ca4f4d2a55c26fc9ca6aeecfaf8d627e
June McEnroe <june@causal.agency>
2019-07-06 17:36:29 -0400

    Add common includes to AGPL template

--- etc/agpl.c
+++ etc/agpl.c
@@ -13,3 +13,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
