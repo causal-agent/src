46237a457ff3c9a813a56f2a442c115bf8610cbc
June McEnroe <june@causal.agency>
2019-07-25 23:16:51 -0400

    Use scheme to style HTMLs

--- bin/html.sh
+++ bin/html.sh
@@ -6,8 +6,11 @@ readonly GiteaURL='https://code.causal.agency/june/src/src/branch/master/bin'
 src=$1
 man=${2:-}
 
-./hi -f html -o document,tab=4 -n "${src}" /dev/null | sed '/<pre/d'
+./hi -f html -o document,tab=4 -n "${src}" /dev/null | sed -e '/<pre/d'
 cat <<- EOF
+	<style>
+	$(./scheme -s | sed -f style.sed)
+	</style>
 	<code><a href="${GiteaURL}/${src}">${src} in git</a></code>
 EOF
 [ -f "${man}" ] && man -P cat "${PWD}/${man}" | ./ttpre
---
+++ bin/style.sed
@@ -0,0 +1,15 @@
+s/[.]bg0/html, &/
+s/[.]fg15/html, &/
+s/[.]fg12/a, &/
+s/[.]fg13/a:visited, &/
+
+s/[.]fg7/.hi.Keyword, &/
+s/[.]fg2/.hi.Macro, &/
+s/[.]fg11/.hi.Tag:focus, &/
+s/[.]fg6/.hi.String, &/
+s/[.]fg14/.hi.Format, &/
+s/[.]fg3/.hi.Interp, &/
+s/[.]fg4/.hi.Comment, &/
+s/[.]fg12/.hi.Todo, &/
+s/[.]fg1 /.hi.DiffOld, &/
+s/[.]fg2/.hi.DiffNew, &/
