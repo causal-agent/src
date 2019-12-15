9c76895de3c2d45a9d0c5408c2a7483ea4a59858
June McEnroe <june@causal.agency>
2019-12-15 17:26:08 -0500

    Link to git. from bin html

--- bin/html.sh
+++ bin/html.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -eu
 
-readonly GiteaURL='https://code.causal.agency/june/src/src/branch/master/bin'
+readonly GitURL='https://git.causal.agency/src/tree/bin'
 
 src=$1
 man=${2:-}
 
 ./hi -f html -o document,tab=4 -n "$src" /dev/null | sed '/<pre/d'
 cat <<- EOF
-	<code><a href="${GiteaURL}/${src}">${src} in git</a></code>
+	<code><a href="${GitURL}/${src}">${src} in git</a></code>
 EOF
 [ -f "$man" ] && man -P cat "${PWD}/${man}" | ./ttpre
 ./hi -f html -o anchor "$src"
