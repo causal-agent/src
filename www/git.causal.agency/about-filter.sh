4aa7728283f4e0ece89437bf7933e8293063c830
June McEnroe <june@causal.agency>
2020-01-08 01:58:44 +0000

    Use mandoc -T html for about-filter
    This also makes source-filter show mdoc source again and removes the
    dependency on ttpre entirely. I copied in the inline stylesheet mandoc
    outputs without -O fragment and added some customizations.

--- www/git.causal.agency/about-filter.sh
+++ www/git.causal.agency/about-filter.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-export LANG=en_US.UTF-8
 case "$1" in
 	(*.[1-9])
-		/usr/bin/mandoc \
-			| /usr/local/libexec/ttpre \
-			| /usr/bin/sed -E \
-				's,([a-z0-9_-]+)[(]([1-9])[)],<a href="\1.\2">&</a>,g'
+		/usr/bin/mandoc -T html -O fragment,man=%N.%S,includes=../tree/%I
 		;;
 	(*)
 		exec /usr/local/libexec/hi -l text -f html
--- www/git.causal.agency/source-filter.sh
+++ www/git.causal.agency/source-filter.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-export LANG=en_US.UTF-8
-case "$1" in
-	(*.[1-9])
-		/usr/local/libexec/about-filter "$@"
-		printf '</code></pre></td><td><pre><code>'
-		;;
-	(*)
-		exec /usr/local/libexec/hi -t -n "$1" -f html -o anchor
-		;;
-esac
+exec /usr/local/libexec/hi -t -n "$1" -f html -o anchor
