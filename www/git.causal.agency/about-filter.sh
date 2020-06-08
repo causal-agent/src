76baaed792d422d3f5ae923767397d5abc127bd9
June McEnroe <june@causal.agency>
2020-06-08 15:47:59 +0000

    Prefix mandoc with exec in about-filter

--- www/git.causal.agency/about-filter.sh
+++ www/git.causal.agency/about-filter.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
 	(*.[1-9])
-		/usr/bin/mandoc -T html -O fragment,man=%N.%S,includes=../tree/%I
+		exec /usr/bin/mandoc -T html -O fragment,man=%N.%S,includes=../tree/%I
 		;;
 	(*)
 		exec /usr/local/libexec/hi -l text -f html
