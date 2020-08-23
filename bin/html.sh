ec966f4bd932379e714aafc78c6fc70117c9a704
June McEnroe <june@causal.agency>
2020-08-23 19:15:46 -0400

    Use scheme for bin html

--- bin/html.sh
+++ bin/html.sh
@@ -6,9 +6,36 @@ readonly GitURL='https://git.causal.agency/src/tree/bin'
 src=$1
 man=${2:-}
 
-./hi -f html -o document,tab=4 -n "$src" /dev/null | sed '/<pre/d'
-cat <<- EOF
-	<code><a href="${GitURL}/${src}">${src} in git</a></code>
+cat <<EOF
+<!DOCTYPE html>
+<title>${src}</title>
+<style>
+$(./scheme -st)
+html {
+	font-family: monospace;
+	color: var(--ansi17);
+	background-color: var(--ansi16);
+}
+a { color: var(--ansi4); }
+a:visited { color: var(--ansi5); }
+pre.hi {
+	-moz-tab-size: 4;
+	tab-size: 4;
+}
+.hi.Keyword { color: var(--ansi7); }
+.hi.Macro { color: var(--ansi2); }
+.hi.Tag { color: inherit; text-decoration: underline; }
+.hi.Tag:target { color: var(--ansi11); outline: none; }
+.hi.String { color: var(--ansi6); }
+.hi.Format { color: var(--ansi14); }
+.hi.Interp { color: var(--ansi3); }
+.hi.Comment { color: var(--ansi4); }
+.hi.Todo { color: var(--ansi12); }
+.hi.DiffOld { color: var(--ansi1); }
+.hi.DiffNew { color: var(--ansi2); }
+</style>
+<a href="${GitURL}/${src}">${src} in git</a>
 EOF
+
 [ -f "$man" ] && man -P cat "${PWD}/${man}" | ./ttpre
 ./hi -f html -o anchor "$src"
