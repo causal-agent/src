287698d0e25c5388de5912254d35628ec56e0bd1
June McEnroe <june@causal.agency>
2020-09-12 07:02:14 +0000

    Enable toc in cgit renderings of man pages
    But keep it disabled for READMEs since they always use non-standard
    sections and the TOC is just distracting there, I think.
    
    Also add the style so its h1 is the same size as the ones inside
    sections...

--- www/git.causal.agency/about-filter.sh
+++ www/git.causal.agency/about-filter.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+options=fragment,man=%N.%S,includes=../tree/%I
+
 case "$1" in
+	(README.[1-9])
+		exec /usr/bin/mandoc -T html -O $options
+		;;
 	(*.[1-9])
-		exec /usr/bin/mandoc -T html -O fragment,man=%N.%S,includes=../tree/%I
+		exec /usr/bin/mandoc -T html -O $options,toc
 		;;
 	(*)
 		exec /usr/local/libexec/hi -l text -f html
