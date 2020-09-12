ad3fc707085b809e08fcb7fb695db7036e9fbb00
June McEnroe <june@causal.agency>
2020-09-11 21:05:02 -0400

    Rewrite install script yet again

--- install.sh
+++ install.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 set -eu
 
-pkgAny='curl htop sl the_silver_searcher tree'
-pkgDarwin="${pkgAny} git neovim pkg-config"
-pkgFreeBSD="${pkgAny} ddate neovim"
-pkgLinux="${pkgAny} bc ctags gdb neovim openssh"
-pkgOpenBSD="${pkgAny} neovim"
-
-Darwin() {
-	if [ ! -d /opt/local ]; then
-		dir=MacPorts-2.6.3
-		tar=${dir}.tar.bz2
-		curl -O "https://distfiles.macports.org/MacPorts/${tar}"
-		tar -x -f $tar
-		(cd $dir && ./configure)
-		make -C $dir
-		sudo make -C $dir install
-		rm -fr $tar $dir
-	fi
-	sudo /opt/local/bin/port selfupdate
-	sudo /opt/local/bin/port -N install $pkgDarwin
-}
+packages='curl htop neovim sl the_silver_searcher tree'
 
 FreeBSD() {
-	pkg install $pkgFreeBSD
-}
-
-Linux() {
-	pacman -Sy --needed $pkgLinux
+	sudo pkg install ddate $packages
 }
 
 OpenBSD() {
-	pkg_add $pkgOpenBSD
+	doas pkg_add $packages
+}
+
+Linux() {
+	sudo pacman -Sy --needed bc ctags gdb openbssh $packages
+}
+
+installMacPorts() {
+	xcode-select --install
+	xcodebuild -license
+	dir=MacPorts-2.6.3
+	tar=${dir}.tar.bz2
+	curl -O "https://distfiles.macports.org/MacPorts/${tar}"
+	tar -x -f $tar
+	(cd $dir && ./configure)
+	make -C $dir
+	sudo make -C $dir install
+	rm -fr $tar $dir
+}
+
+Darwin() {
+	[ -d /opt/local ] || installMacPorts
+	sudo /opt/local/bin/port selfupdate
+	sudo /opt/local/bin/port -N install git pkgconfig $packages
 }
 
 $(uname)
