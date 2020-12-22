e4121f04398c9b2714a68ac1292ae1116b70587b
June McEnroe <june@causal.agency>
2020-12-22 16:41:11 -0500

    Limit prune.sh to ~/.config ~/.local
    Otherwise macOS gets very mad as it tries to access a bunch of protected
    directories.

--- prune.sh
+++ prune.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-find -L ~ -type l -lname "${PWD}/*" | while read -r link; do
+find -L ~/.config ~/.local -type l -lname "${PWD}/*" | while read -r link; do
 	echo "$link"
 	rm "$link"
 done
