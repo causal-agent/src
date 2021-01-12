b037118b31ece700232ae31fafde32a8787fef11
June McEnroe <june@causal.agency>
2021-01-12 16:55:34 -0500

    Use mandoc and htagml for bin html

--- bin/html.sh
+++ bin/html.sh
@@ -10,32 +10,57 @@ cat <<EOF
 <!DOCTYPE html>
 <title>${src}</title>
 <style>
-$(./scheme -st)
+$(./scheme -s)
+
+table.head, table.foot { width: 100%; }
+td.head-rtitle, td.foot-os { text-align: right; }
+td.head-vol { text-align: center; }
+div.Pp { margin: 1ex 0ex; }
+div.Nd, div.Bf, div.Op { display: inline; }
+span.Pa, span.Ad { font-style: italic; }
+span.Ms { font-weight: bold; }
+dl.Bl-diag > dt { font-weight: bold; }
+code.Nm, code.Fl, code.Cm, code.Ic, code.In, code.Fd, code.Fn,
+code.Cd { font-weight: bold; font-family: inherit; }
+
+table { border-collapse: collapse; }
+table.Nm code.Nm { padding-right: 1ch; }
+table.foot { margin-top: 1em; }
+
 html {
+	line-height: 1.25em;
 	font-family: monospace;
-	color: var(--ansi17);
-	background-color: var(--ansi16);
-}
-a { color: var(--ansi4); }
-a:visited { color: var(--ansi5); }
-pre.hi {
+	background-color: var(--ansi0);
+	color: var(--ansi7);
 	-moz-tab-size: 4;
 	tab-size: 4;
 }
-.hi.Keyword { color: var(--ansi7); }
-.hi.Macro { color: var(--ansi2); }
-.hi.Tag { color: inherit; text-decoration: underline; }
-.hi.Tag:target { color: var(--ansi11); outline: none; }
-.hi.String { color: var(--ansi6); }
-.hi.Format { color: var(--ansi14); }
-.hi.Interp { color: var(--ansi3); }
-.hi.Comment { color: var(--ansi4); }
-.hi.Todo { color: var(--ansi12); }
-.hi.DiffOld { color: var(--ansi1); }
-.hi.DiffNew { color: var(--ansi2); }
+body {
+	max-width: 80ch;
+	margin: 1em auto;
+	padding: 0 1ch;
+}
+a { color: var(--ansi4); }
+a:visited { color: var(--ansi5); }
+a.permalink, a.tag {
+	color: var(--ansi6);
+	text-decoration: none;
+}
+a.permalink code:target,
+h1.Sh:target a.permalink,
+h2.Ss:target a.permalink,
+a.tag:target {
+	color: var(--ansi3);
+	outline: none;
+}
 </style>
-<a href="${GitURL}/${src}">${src} in git</a>
 EOF
 
-[ -f "$man" ] && man -P cat "${PWD}/${man}" | ./ttpre
-./hi -t -f html -o anchor "$src"
+opts='fragment'
+[ "${man}" = "README.7" ] && opts="${opts},man=%N.html"
+mandoc -T html -O "${opts}" "${man}"
+cat <<EOF
+<p>
+<a href="${GitURL}/${src}">${src} in git</a>
+EOF
+./htagml -p -f htmltags "${src}"
