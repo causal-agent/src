ff9f7f414ee1e74d0a63d6072c5d2158e017f09e
June McEnroe <june@causal.agency>
2021-01-20 03:49:40 +0000

    Use mtags in source-filter

--- www/git.causal.agency/source-filter.sh
+++ www/git.causal.agency/source-filter.sh
@@ -2,17 +2,21 @@
 set -eu
 
 ctags=/usr/bin/ctags
+mtags=/usr/local/libexec/mtags
 hilex=/usr/local/libexec/hilex
 htagml=/usr/local/libexec/htagml
 
 case "$1" in
-	(*.[chlmy])
+	(*.[chlmy]|Makefile|*.mk|*.[1-9])
 		tmp=$(mktemp -d -t source-filter)
 		trap 'rm -fr "${tmp}"' EXIT
 		cd "${tmp}"
 		cat >"$1"
 		touch tags
-		$ctags -w "$1"
+		case "$1" in
+			(*.[chlmy]) $ctags -w "$1";;
+			(*) $mtags "$1";;
+		esac
 		$hilex -f html "$1" | $htagml -i "$1"
 		;;
 	(*)
