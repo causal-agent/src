a8dcf1e99a0811e90db8ba88fbf18f2a79e43069
June McEnroe <june@causal.agency>
2021-04-11 22:26:16 -0400

    Simplify rfctags
    I wasn't thinking perl enough.

--- doc/rfc/rfctags.pl
+++ doc/rfc/rfctags.pl
@@ -4,22 +4,18 @@ use warnings;
 use open ':encoding(ISO-8859-1)';
 
 ($,, $\) = ("\t", "\n");
-for my $rfc (@ARGV) {
-	open my $handle, '<', $rfc or die "${rfc}: $!";
-	while (<$handle>) {
-		chomp;
-		# Section headings
-		if (/^([\d.]+|[A-Z][.])\s+([^\t]+)?/) {
-			print $1, $rfc, $.;
-			print $2, $rfc, $. if $2;
-			print $1, $rfc, $. if $1 =~ /^([\d.]+)[.]$/;
-		}
-		# References
-		if (/^\s*(\[[\w-]+\])\s{2,}/) {
-			print $1, $rfc, $.;
-			print "\\$1", $rfc, $.; # vim ^] prepends \ to [
-		}
+while (<>) {
+	chomp;
+	# Section headings
+	if (/^([\d.]+|[A-Z][.])\s+([^\t]+)?/) {
+		print $1, $ARGV, $.;
+		print $2, $ARGV, $. if $2;
+		print $1, $ARGV, $. if $1 =~ /^([\d.]+)[.]$/;
 	}
-	die "${rfc}: $!" if $!;
-	close $handle;
+	# References
+	if (/^\s*(\[[\w-]+\])\s{2,}/) {
+		print $1, $ARGV, $.;
+		print "\\$1", $ARGV, $.; # vim ^] prepends \ to [
+	}
+	close ARGV if eof;
 }
