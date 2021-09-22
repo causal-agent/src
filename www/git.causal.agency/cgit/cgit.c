b66b864a89e0a956e9d5d8909ccb3084df3c273e
June McEnroe <june@causal.agency>
2021-09-22 15:40:24 +0000

    Remove default favicon
    I hate these things and also this one sucks.

--- www/git.causal.agency/cgit/cgit.c
+++ www/git.causal.agency/cgit/cgit.c
@@ -378,7 +378,7 @@ static void prepare_context(void)
 	ctx.cfg.commit_sort = 0;
 	ctx.cfg.css = "/cgit.css";
 	ctx.cfg.logo = "/cgit.png";
-	ctx.cfg.favicon = "/favicon.ico";
+	ctx.cfg.favicon = NULL;
 	ctx.cfg.local_time = 0;
 	ctx.cfg.enable_http_clone = 1;
 	ctx.cfg.enable_index_owner = 1;
