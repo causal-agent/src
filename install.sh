d37a3260697efbf6d8fa4d7c1eb0bbcbb1b6a55f
June McEnroe <june@causal.agency>
2022-01-02 20:26:23 -0500

    Remove attempt to automatically install MacPorts
    It didn't really work on a new machine, and the version needs to
    be updated. I'll just do it manually.

--- install.sh
+++ install.sh
@@ -26,21 +26,7 @@ Linux() {
 	sudo pacman -Sy --needed bc ctags gdb openssh vi $packages
 }
 
-installMacPorts() {
-	xcode-select --install
-	xcodebuild -license
-	dir=MacPorts-2.6.3
-	tar=${dir}.tar.bz2
-	curl -O "https://distfiles.macports.org/MacPorts/${tar}"
-	tar -x -f $tar
-	(cd $dir && ./configure)
-	make -C $dir
-	sudo make -C $dir install
-	rm -fr $tar $dir
-}
-
 Darwin() {
-	[ -d /opt/local ] || installMacPorts
 	sudo /opt/local/bin/port selfupdate
 	sudo /opt/local/bin/port -N install git mandoc nvi pkgconfig $packages
 	sudo mkdir -p /opt/local/etc/select/man
